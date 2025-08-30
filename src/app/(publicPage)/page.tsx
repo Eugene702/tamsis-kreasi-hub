6b967b030d218bca305341854ee8b90ccb3f512c
Eugene702 <eugenefeilianputrarangga@gmail.com>
2025-08-30 20:11:12 +0700

    Feat: Fix Round

--- src/app/(publicPage)/page.tsx
+++ src/app/(publicPage)/page.tsx
@@ -25,7 +25,7 @@ const Page = () => {
               placeholder="Kreativitas apa yang ingin Anda temukan hari ini?"
               aria-label="Cari kreativitas"
             />
-            <button className="btn bg-emerald-700 hover:bg-emerald-600 btn-circle min-h-10 h-10 w-10" aria-label="Cari">
+            <button className="btn bg-emerald-700 hover:bg-emerald-600 btn-circle !rounded-full min-h-10 h-10 w-10" aria-label="Cari">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-white w-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
               </svg>
@@ -45,7 +45,7 @@ const Page = () => {
             <Link
               href="/category/web-development"
               key={i}
-              className="btn btn-xs sm:btn-sm btn-outline rounded-full whitespace-nowrap hover:btn-primary hover:text-primary-content transition"
+              className="btn btn-xs sm:btn-sm btn-outline !rounded-full whitespace-nowrap hover:btn-primary hover:text-primary-content transition"
             >
               Web Development
             </Link>
@@ -59,7 +59,7 @@ const Page = () => {
         {Array.from({ length: 8 }).map((_, i) => <CardContent key={i} />)}
       </div>
       <div className="mt-12 flex justify-center">
-        <button className="btn btn-outline btn-sm sm:btn-md rounded-full">Load More</button>
+        <button className="btn btn-outline btn-sm sm:btn-md !rounded-full">Load More</button>
       </div>
     </section>
   </Fragment>
